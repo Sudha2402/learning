9044c96457f4a03f1cb141d6bbb3e5609909410c
Sudha2402 <sudhakumarichauhan24@gmail.com>
2025-06-29 16:10:57 +0530

    Add files via upload

---
+++ website/worker1.js
@@ -0,0 +1,10 @@
+onmessage = function(event) {
+    console.log('Received message from worker in worker.js:', event.data);//data used not the value //value only for input element////Received message from worker in worker.js : Hello, worker!
+ //   this.postMessage(event.data);
+    postMessage(event.data); //undefined for document.write event.data
+  //  postMessage(data);//data is not defined
+ // var data=event.data;
+// postMessage(data);
+    };
+    //terminate the worker
+    // worker.terminate();
